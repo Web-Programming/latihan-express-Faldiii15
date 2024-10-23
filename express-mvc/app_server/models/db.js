c567baac64e8e981919a98ac32227079c535169f
Faldiii <rifaldif888@gmail.com>
2024-10-23 08:36:28 +0700

    Commit-revisi

--- express-mvc/app_server/models/db.js
+++ express-mvc/app_server/models/db.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
 let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
+require("./mahasiswa");
 
 mongoose.connect(dbURI,{
     // useNewUrlParser: true
@@ -12,6 +13,6 @@ mongoose.connection.on("error", (error) =>{
 });
 mongoose.connection.on("disconnected",() =>{
     console.log("Disconnected from MongoDB");
-    require("./mahasiswa");
+     
 
 });
